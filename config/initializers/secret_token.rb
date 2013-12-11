de9116230efe88722fe9dd8bcc872c7f6f11f4cc
ThomasDebek <tom.debek@gmail.com>
2013-12-11 12:31:49 +0100

    Adding app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniBank::Application.config.secret_key_base = 'c1ac2629c6446be76d6391d0720f1b7cce02ab592e7bef1324553c0ab7998813093d51524ef7be2ad517b04705b23d1b839a283d7aa17ef93682bfcb39d0596b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MiniBank::Application.config.session_store :cookie_store, key: '_Mini_Bank_session'
