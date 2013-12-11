0c9f2db4d75ced9ba97df6874c3a4aa7e1e3d917
ThomasDebek <tom.debek@gmail.com>
2013-12-11 13:23:30 +0100

    Test models client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,16 @@
 class Client < ActiveRecord::Base
   validates :name, :surname, length: { minimum: 2 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
-  validates :phone_number, format: { with: /\d{3}-\d{3}-\d{3}/, message: "bad format" }
+  validates :phone, format: { with: /\d{3}-\d{3}-\d{3}/, message: "bad format" }
+
+
+  def to_s
+    "#{name} #{surname} (#{id})"
+  end
+
+
+
+
+
+
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid" do                        # spr. czy wystepuje walidacjia
+    c = Client.new                       # tworzymy klienta
+    assert_equal false, c.valid?         # assercja oczekuj false
+    assert c.invalid?                    # drugi sposób zapisu
+  end
+
+  test "name, surname presence" do       # spr. czy imie i naz.. nie jest puste
+    c = Client.new
+    c.valid?
+    assert c.errors[:name].any?          # spr czy sa bledy na imieniu
+    assert c.errors[:surname].any?       # i nazwisku
+    c.name = 'Jan'
+    c.surname = 'Kowalski'
+    c.valid?
+    assert c.errors[:name].empty?        # spr. czy nie są puste
+    assert c.errors[:surname].empty?
+  end
+
+  test "to_s" do                              # przetestujmy metode to_s
+    c = Client.create(name:'Jan',             # upewnijmy sie ze w nawiasach
+                      surname:'Nowak',        # bedzie wstawiona jaks liczba
+                      email: 'jan@wp.pl',
+                      phone: '234-234-234')
+     assert_match /Jan Nowak \(\d+\)/, c.to_s # assrcja korzystająca z wyrazenia reguralnego
+  end
+
 end
