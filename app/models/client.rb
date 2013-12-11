18d3366635386624ec3124323c164066e6038a58
ThomasDebek <tom.debek@gmail.com>
2013-12-11 12:48:16 +0100

    Frist validates Client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
 class Client < ActiveRecord::Base
   validates :name, :surname, length: { minimum: 2 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+  validates :phone_number, format: { with: /\d{3}-\d{3}-\d{3}/, message: "bad format" }
 end
