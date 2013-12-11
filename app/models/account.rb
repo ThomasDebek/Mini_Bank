3ccab0c167531cfa35970594255b97afa23fe3b2
ThomasDebek <tom.debek@gmail.com>
2013-12-11 14:06:51 +0100

    Adding validates, methods account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,19 @@
 class Account < ActiveRecord::Base
   belongs_to :client
+
+                                                          # saldo musi byc wartoscia numeryczną
+  validates :balance, numericality: { only_integer: true} # i tylko typ integer
+
+
+  before_create :set_number
+
+  def to_s                                                # konto zaraz bedzie mialo nr
+     "[#{number}] #{balance} zl"                                                         # i chcemy wysietlic stan konta
+  end
+
+  protected                                               # chce zeby model sam wygenerowal sobie nr konta
+  def set_number                                          # i wylosowal liczbe z tego zakresu
+    self.number = rand 1_000_000_000                      # i podstawil do zmiennej
+  end                                                     # ale musze uzyc collbecka przed tym
+
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ActiveRecord::Base
+  has_many :accounts                        # mówimy ze klient ma wiele kont
+
   validates :name, :surname, length: { minimum: 2 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
   validates :phone, format: { with: /\d{3}-\d{3}-\d{3}/, message: "bad format" }
