9ed53211c88d286d7c4864cd3fa1f92e72eb60a3
ThomasDebek <tom.debek@gmail.com>
2013-12-16 13:34:17 +0100

    Rails g resources transaction -s

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 MiniBank::Application.routes.draw do
   root to: 'clients#index'
+
   resources :clients do
     resources :accounts
+  end
+
+  resources :accounts do
+    resources :transactions
   end
 
 
