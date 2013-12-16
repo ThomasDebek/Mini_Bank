b71a71b10be896bdba3fe81e8f4bc673ee1687cb
ThomasDebek <tom.debek@gmail.com>
2013-12-16 15:48:54 +0100

    Adding methods to create new accounts, and views

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,29 @@
 class TransactionsController < ApplicationController
+
+  before_filter :find_account
+
+  def new
+    @transaction = @account.transactions.new
+  end
+
+  def create
+    @transaction = @account.transactions.new(transaction_params)
+    if @transaction.save
+      msg = 'Transakcja przebiegla pomyślnie'
+      redirect_to client_account_path(@client, @account), notice: msg
+
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+    def find_account
+      @account = Account.find(params[:account_id])
+      @client = @account.client
+    end
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :descr)
+  end
 end
