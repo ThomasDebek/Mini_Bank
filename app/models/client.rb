866c0becf85d32f4799ffdd727b9ca9cc5d3761c
ThomasDebek <tom.debek@gmail.com>
2013-12-20 15:42:24 +0100

    Rails g migration add_password_to_clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,8 @@
 class Client < ActiveRecord::Base
   has_many :accounts                        # mówimy ze klient ma wiele kont
 
+  has_secure_password
+
   validates :name, :surname, length: { minimum: 2 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
   validates :phone, format: { with: /\d{3}-\d{3}-\d{3}/, message: "bad format" }
