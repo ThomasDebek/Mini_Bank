42f5e2c490967b856bf6b8998e53703963e7b301
ThomasDebek <tom.debek@gmail.com>
2013-12-21 13:11:15 +0100

    Adding methods - the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  def new       # wyswietli formularz do logowania
+  end
+
+  def create                                      # przyjmnie parametry z tego formularza
+    client = Client.find_by_email(params[:email]) # szukam klienta po emailu
+    if client && client.authenticate(params[:password])
+      session[:client_id] = client.id
+      redirect_to client_accounts_path(client), notice: 'Witamy :) '
+    else
+      flash.now.alert 'Niestety :('
+      render action: 'new'
+    end
+  end
+
+  def destroy   # pozwoli wylogowac uzytkownika
+    session[:client_id] = nil                         # czyszcze sessjie
+    redirect_to root_path, notice: 'Do zobaczenia...' # przekierowuje na główną strone serwisu
+  end
+
 end
